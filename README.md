360a5f341056de4d1ee434e98e40585d7c471d1e
tarasowski <dimitri+github@tarasowski.de>
2021-10-25 09:50:07 +0200

    feat: Finished sveltekit poc

---
+++ README.md
@@ -0,0 +1,4 @@
+# create-svelte
+
++ the description how to make the app working
++ https://dev.to/robertobutti/how-to-start-building-your-static-website-with-svelte-and-tailwindcss-hbk
---
+++ src/routes/about.svelte
@@ -0,0 +1,9 @@
+<section class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
+  <h1 class="pt-10 text-7xl">About page</h1>
+  <div class="font-serif pt-12 text-2xl">Ciao!</div>
+  <div class="font-serif pt-12 text-3xl">Ciao!</div>
+<br>
+
+<a href="/">Go back home</a>
+
+</section>
